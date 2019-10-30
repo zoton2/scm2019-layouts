51c728746efe5426ce19fd1d6b216976defc0c08
zoton2 <zoton2@gmail.com>
2019-10-30 05:33:04 +0000

    Allow team players to cycle
    Not sure if we needed this but it was a simple addition

--- src/graphics/_misc/components/Player.vue
+++ src/graphics/_misc/components/Player.vue
@@ -25,31 +25,45 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'; // eslint-disable-line object-curly-newline, max-len
 import { State } from 'vuex-class';
 import { RunDataActiveRun } from '../../../../../nodecg-speedcontrol/types';
 
 @Component
 export default class Player extends Vue {
-  @State runDataActiveRun!: RunDataActiveRun;
+  @State('runDataActiveRun') run!: RunDataActiveRun;
   @Prop(Boolean) readonly small!: boolean;
   @Prop({ default: 1 }) readonly team!: number;
+  timeout?: number;
+  teamI = 0;
+  index = 0;
+  name: string | null = null;
 
-  get name(): string | undefined {
-    const coop = this.runDataActiveRun && this.runDataActiveRun.teams.length === 1
-      && this.runDataActiveRun.teams[0].players.length >= 2;
+  @Watch('run', { immediate: true })
+  onRunChange(val: RunDataActiveRun): void {
+    window.clearTimeout(this.timeout);
+    this.teamI = this.team - 1;
+    this.index = 0;
+    this.name = null;
+    const coop = !!(val && val.teams.length === 1 && val.teams[0].players.length >= 2);
 
-    if (this.runDataActiveRun) {
-      if (coop && this.runDataActiveRun.teams[0]
-      && this.runDataActiveRun.teams[0].players[this.team - 1]) {
-        return this.runDataActiveRun.teams[0].players[this.team - 1].name;
-      }
-      if (!coop && this.runDataActiveRun.teams[this.team - 1]
-      && this.runDataActiveRun.teams[this.team - 1].players[0]) {
-        return this.runDataActiveRun.teams[this.team - 1].players[0].name;
+    if (val) {
+      if (coop && val.teams[0].players[this.teamI]) {
+        this.name = val.teams[0].players[this.teamI].name;
+      } else if (!coop && val.teams[this.teamI] && val.teams[this.teamI].players.length) {
+        this.showNextName();
       }
     }
-    return undefined;
+  }
+
+  showNextName(): void {
+    if (!this.run) {
+      return;
+    }
+    const { players } = this.run.teams[this.teamI];
+    this.name = players[this.index].name;
+    this.timeout = window.setTimeout(() => this.showNextName(), 30 * 1000);
+    this.index = (players.length <= this.index + 1) ? 0 : this.index + 1;
   }
 }
 </script>
