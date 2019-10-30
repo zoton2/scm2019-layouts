427af3e48b1767e3fb35ec581e5055e9a46de01f
zoton2 <zoton2@gmail.com>
2019-10-30 04:37:54 +0000

    Actually implement sponsor logo rotation

--- src/graphics/_misc/components/SponsorLogos.vue
+++ src/graphics/_misc/components/SponsorLogos.vue
@@ -1,10 +1,13 @@
 <template>
   <div :style="{ position: 'fixed' }">
-    <img
-      v-if="sponsorLogos.length"
-      :style="{ width: '100%', height: '100%' }"
-      :src="sponsorLogos[0].url"
-    >
+    <transition name="fade">
+      <img
+        v-if="logos.length"
+        :key="currentSum"
+        :style="{ position: 'absolute', width: '100%', height: '100%', 'object-fit': 'contain' }"
+        :src="currentURL"
+      >
+    </transition>
   </div>
 </template>
 
@@ -15,6 +18,24 @@ import { Asset } from '../../../../types';
 
 @Component
 export default class SponsorLogos extends Vue {
-  @State('assets:sponsor-logos') sponsorLogos!: Asset[];
+  @State('assets:sponsor-logos') logos!: Asset[];
+  index = 0;
+  currentSum = '';
+  currentURL = '';
+
+  created(): void {
+    this.showNextLogo();
+  }
+
+  showNextLogo(): void {
+    if (!this.logos.length) {
+      setTimeout(this.showNextLogo, 5 * 1000);
+      return;
+    }
+    this.currentSum = this.logos[this.index].sum;
+    this.currentURL = this.logos[this.index].url;
+    setTimeout(this.showNextLogo, 60 * 1000);
+    this.index = (this.logos.length <= this.index + 1) ? 0 : this.index + 1;
+  }
 }
 </script>
