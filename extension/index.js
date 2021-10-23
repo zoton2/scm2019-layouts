9db1f42951dc20c41702baea8939eee3d8a93e13
zoton2 <zoton2@gmail.com>
2021-10-24 00:41:01 +0100

    Remove built files

--- extension/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* eslint global-require: off */
-const nodecg_1 = require("./util/nodecg");
-module.exports = (nodecg) => {
-    nodecg_1.set(nodecg);
-    require('./util/obs'); // Make sure OBS connection is setup.
-    require('./tracker');
-    require('./layouts');
-};
